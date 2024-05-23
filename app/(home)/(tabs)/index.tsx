e52eb2bc6ec3312b21570e91041ccee61ee826e2
blnche <peltierblanche@gmail.com>
2024-05-23 17:34:43 +0200

    get task by user done

--- app/(home)/(tabs)/index.tsx
+++ app/(home)/(tabs)/index.tsx
@@ -1,17 +1,63 @@
 import { Stack } from 'expo-router';
-import { StyleSheet, View } from 'react-native';
+import { Button, FlatList, StyleSheet, View } from 'react-native';
 import { ScreenContent } from '~/components/ScreenContent';
-import { Text } from 'tamagui';
+import { ListItem, Text } from 'tamagui';
+import { useAuth } from '~/providers/AuthProvider';
+import React, { useEffect, useState } from 'react';
+import { supabase } from '~/utils/supabase';
+
+type Todo = {
+  id: number
+  task: string
+  is_complete: boolean
+  inserted_at: Date
+  profile_user_id: string
+}
 
 export default function MainTabScreen() {
 
+  const { user } = useAuth()
+  const [todos, setTodos] = useState<Array<Todo>>([])
+  const [newTodo, setNewTodo] = useState<string>('')
+
+  useEffect(() => {
+    fetchTodos()
+  }, [])
+
+  const fetchTodos = async () => {
+    const { data: todos, error } = await supabase
+    .from<Todo>('todos')
+    .select('*')
+    .order('id', {ascending: false})
+
+    if (error) console.log('error', error)
+    else setTodos(todos!)
+  }
+
   return (
     <>
-      <Stack.Screen options={{ title: 'Home screen' }} />
-      {/* <View style={styles.container}> */}
-        {/* <ScreenContent path="app/(tabs)/index.tsx" title="Today's Tasks" /> */}
-        <Text>MainTab</Text>
-      {/* </View> */}
+      <Stack.Screen options={{ title: 'Tasks' }} />
+      <View style={styles.container}>
+        <Text>Tasks</Text>
+        <FlatList 
+          scrollEnabled={true}
+          data={todos}
+          keyExtractor={(item) => `${item.id}`}
+          renderItem={({ item: todo }) => (
+            
+            <View
+              style={[
+                { display: 'flex', flexDirection: 'row', justifyContent: 'space-between' },
+              ]}
+            >
+              
+              <Text>
+                {todo.task}
+              </Text>
+            </View>
+          )}
+        />
+      </View>
     </>
   );
 }
